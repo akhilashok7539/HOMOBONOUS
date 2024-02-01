92d952cde10474c83e56ea986ae8ea819f876daa
akhilashok7539 <akhilashok7539@gmail.com>
2024-02-01 22:46:42 +0530

    review changes

--- js/form.js
+++ js/form.js
@@ -26,6 +26,19 @@ document.getElementById("myFormMobile").addEventListener("submit", function (eve
 });
 
 
+document.getElementById("myFormNewsLetterMobile").addEventListener("submit", function (event) {
+    event.preventDefault(); // Prevent the default form submission
+
+    // Get form data
+    var formData = new FormData(this);
+    var data = {};
+    formData.forEach(function (value, key) {
+        data[key] = value;
+    });
+
+    // Call a function to submit the data to Google Sheets
+    submitToGoogleSheetsNewsLetter(data);
+});
 document.getElementById("myFormNewsLetter").addEventListener("submit", function (event) {
     event.preventDefault(); // Prevent the default form submission
 
@@ -37,7 +50,7 @@ document.getElementById("myFormNewsLetter").addEventListener("submit", function 
     });
 
     // Call a function to submit the data to Google Sheets
-    submitToGoogleSheets(data);
+    submitToGoogleSheetsNewsLetter(data);
 });
 
 
@@ -55,10 +68,11 @@ function submitForm(event) {
     submitToGoogleSheets(data);
 }
 
-
+// form email notifications
 function submitToGoogleSheets(data) {
     // Use Google Apps Script to handle form submission to Google Sheets
-    var scriptURL = 'https://script.google.com/macros/s/AKfycbwPvkEDUvhHIPmhTfPwuaxth1czYGAO3R31scxIhFE6oO1MUd3KifYXc8FxRedbC0p1/exec';
+    // var scriptURL = 'https://script.google.com/macros/s/AKfycbwPvkEDUvhHIPmhTfPwuaxth1czYGAO3R31scxIhFE6oO1MUd3KifYXc8FxRedbC0p1/exec';
+    var scriptURL = 'https://script.google.com/macros/s/AKfycbyyJVs6B2nj89lGMDIsRbi5e7uS1vcDT-Jk96y3d1UNbIDjSfsSgpibY5IJDZYaBdY/exec';
     var payload = JSON.stringify(data);
 
     fetch(scriptURL, {
@@ -80,20 +94,30 @@ function submitToGoogleSheets(data) {
             console.error('Error:', error)
         });
 }
-function showLoader() {
-    var loader = document.getElementById('loader');
-    loader.style.display = 'inline-block';
+// for news letter
+function submitToGoogleSheetsNewsLetter(data) {
+    // Use Google Apps Script to handle form submission to Google Sheets
+    var scriptURL = 'https://script.google.com/macros/s/AKfycbwG5Ee85d2QXvhVIEKR_Mrp3o0sG5WubVn4pOu1fH0bFpwru7UvjVcTD5boQ4XbYbQbCQ/exec';
+    var payload = JSON.stringify(data);
 
-    var submitBtn = document.getElementById('submitBtn');
-    submitBtn.setAttribute('disabled', 'true'); // Disable the button while loading
-}
+    fetch(scriptURL, {
+        method: 'POST',
+        mode: 'no-cors',
+        headers: {
+            'Content-Type': 'application/json',
+        },
+        body: payload,
+    })
+        .then(response => {
+            console.log('Success!', response)
+            alert("Subscribe Email")
+            showToaster();
 
-function hideLoader() {
-    var loader = document.getElementById('loader');
-    loader.style.display = 'none';
-
-    var submitBtn = document.getElementById('submitBtn');
-    submitBtn.removeAttribute('disabled'); // Enable the button after loading
+        })
+        .catch(error => {
+         
+            console.error('Error:', error)
+        });
 }
 
 function showToaster() {
